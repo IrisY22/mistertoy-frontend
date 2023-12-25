c33d8b9fa51b78213b6e7903b5528d0b2291b2da
Iris Yatskaer <iriska1160@gmail.com>
2023-12-25 20:53:44 +0200

    Refactore frontend connected to backend

--- src/services/toy.service.js
+++ src/services/toy.service.js
@@ -28,85 +28,85 @@ export const toyService = {
 //   return toys;
 // }
 
-function query(filterBy) {
-  const { sortBy } = filterBy;
-  return storageService.query(TOYS_KEY).then((toys) => {
-    if (filterBy.txt) {
-      const regex = new RegExp(filterBy.txt, "i");
-      toys = toys.filter((toy) => regex.test(toy.name));
-    }
-    if (filterBy.maxPrice) {
-      toys = toys.filter((toy) => toy.price <= filterBy.maxPrice);
-    }
-    if (filterBy.inStock !== "All") {
-      toys = toys.filter((toy) =>
-        filterBy.inStock === "In stock" ? toy.inStock : !toy.inStock
-      );
-    }
+// function query(filterBy) {
+//   const { sortBy } = filterBy;
+//   return storageService.query(TOYS_KEY).then((toys) => {
+//     if (filterBy.txt) {
+//       const regex = new RegExp(filterBy.txt, "i");
+//       toys = toys.filter((toy) => regex.test(toy.name));
+//     }
+//     if (filterBy.maxPrice) {
+//       toys = toys.filter((toy) => toy.price <= filterBy.maxPrice);
+//     }
+//     if (filterBy.inStock !== "All") {
+//       toys = toys.filter((toy) =>
+//         filterBy.inStock === "In stock" ? toy.inStock : !toy.inStock
+//       );
+//     }
 
-    return _getSortedToys(sortBy, toys);
-  });
-}
-
-function _getSortedToys(sortBy, toys) {
-  let sortedToys;
-  if (sortBy === "txt") {
-    sortedToys = toys.sort((a, b) => {
-      const toyA = a.txt.toUpperCase();
-      const toyB = b.txt.toUpperCase();
-      if (toyA < toyB) {
-        return -1;
-      }
-      if (toyA > toyB) {
-        return 1;
-      }
-      return 0;
-    });
-  } else {
-    sortedToys = toys.sort((a, b) => a.createdAt - b.createdAt);
-  }
-  return sortedToys;
-}
-
-function getById(id) {
-  return storageService.get(TOYS_KEY, id);
-}
-
-async function save(toy) {
-  if (toy._id) {
-    const savedToy = await storageService.put(TOYS_KEY, toy);
-    return savedToy;
-  } else {
-    toy.createdAt = Date.now();
-    const savedToy_1 = await storageService.post(TOYS_KEY, toy);
-    return savedToy_1;
-  }
-}
-
-async function remove(toyId) {
-  await storageService.remove(TOYS_KEY, toyId);
-  return toyId;
-}
-
-// function query(filterBy = {}) {
-//   return httpService.get(BASE_URL, filterBy);
+//     return _getSortedToys(sortBy, toys);
+//   });
 // }
 
-// function getById(toyId) {
-//   return httpService.get(BASE_URL + toyId);
-// }
-
-// function remove(toyId) {
-//   return httpService.delete(BASE_URL + toyId);
-// }
-
-// function save(toy) {
-//   if (toy._id) {
-//     return httpService.put(BASE_URL, toy);
+// function _getSortedToys(sortBy, toys) {
+//   let sortedToys;
+//   if (sortBy === "txt") {
+//     sortedToys = toys.sort((a, b) => {
+//       const toyA = a.txt.toUpperCase();
+//       const toyB = b.txt.toUpperCase();
+//       if (toyA < toyB) {
+//         return -1;
+//       }
+//       if (toyA > toyB) {
+//         return 1;
+//       }
+//       return 0;
+//     });
 //   } else {
-//     return httpService.post(BASE_URL, toy);
+//     sortedToys = toys.sort((a, b) => a.createdAt - b.createdAt);
+//   }
+//   return sortedToys;
+// }
+
+// function getById(id) {
+//   return storageService.get(TOYS_KEY, id);
+// }
+
+// async function save(toy) {
+//   if (toy._id) {
+//     const savedToy = await storageService.put(TOYS_KEY, toy);
+//     return savedToy;
+//   } else {
+//     toy.createdAt = Date.now();
+//     const savedToy_1 = await storageService.post(TOYS_KEY, toy);
+//     return savedToy_1;
 //   }
 // }
+
+// async function remove(toyId) {
+//   await storageService.remove(TOYS_KEY, toyId);
+//   return toyId;
+// }
+
+function query(filterBy = {}) {
+  return httpService.get(BASE_URL, filterBy);
+}
+
+function getById(toyId) {
+  return httpService.get(BASE_URL + toyId);
+}
+
+function remove(toyId) {
+  return httpService.delete(BASE_URL + toyId);
+}
+
+function save(toy) {
+  if (toy._id) {
+    return httpService.put(BASE_URL, toy);
+  } else {
+    return httpService.post(BASE_URL, toy);
+  }
+}
 
 function getEmptyToy() {
   return {
