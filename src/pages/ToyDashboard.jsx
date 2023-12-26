467f4ea3b4fb1f646116533a70ac1b1d65208d67
Iris Yatskaer <iriska1160@gmail.com>
2023-12-26 19:37:20 +0200

    Add charts

--- src/pages/ToyDashboard.jsx
+++ src/pages/ToyDashboard.jsx
@@ -3,7 +3,7 @@ import { Chart as Chartjs } from "chart.js/auto";
 import { toyService } from "../services/toy.service.js";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import { loadToys } from "../store/actions/toyActions.js";
 
@@ -13,53 +13,115 @@ export function ToyDashboard() {
   const chartMap = new Map();
   const labels = toyService.getLabels();
   labels.forEach((label) => chartMap.set(label, 0));
+  const [currChar, setCurrChar] = useState(showByPrice());
 
   useEffect(() => {
     if (!toys || toys.length === 0) {
       loadToys();
+    } else {
+      setCurrChar(showByPrice());
     }
-  }, []);
+  }, [toys]);
 
-  toys.map((toy) =>
-    toy.labels.forEach((label) => updateLabel(label, toy.price))
-  );
+  function showByPrice() {
+    toys.map((toy) =>
+      toy.labels.forEach((label) => updateLabel(label, toy.price))
+    );
 
-  function updateLabel(label, price) {
-    const currentPrice = chartMap.get(label);
-    chartMap.set(label, price + currentPrice);
+    function updateLabel(label, price) {
+      const currentPrice = chartMap.get(label);
+      chartMap.set(label, price + currentPrice);
+    }
+    const priceArr = [];
+    chartMap.forEach((value, key) => {
+      priceArr.push(value);
+    });
+    return {
+      labels: labels,
+      datasets: [
+        {
+          label: "Price",
+          data: priceArr,
+          backgroundColor: [
+            " #ff1a1a",
+            " #ff4dff",
+            " #4dd2ff",
+            " #33ff77",
+            " #ffff33",
+            " #d2a679",
+            " #4d4dff",
+            " #ffa64d",
+            " #a3a3c2 ",
+            " #ff99ff ",
+          ],
+          borderColor: " #29293d",
+          borderWidth: 1,
+        },
+      ],
+    };
   }
-  const priceArr = [];
-  chartMap.forEach((value, key) => {
-    priceArr.push(value);
-  });
 
-  const data = {
-    labels: labels,
-    datasets: [
-      {
-        label: "Price",
-        data: priceArr,
-        backgroundColor: [
-          " #ff1a1a",
-          " #ff4dff",
-          " #4dd2ff",
-          " #33ff77",
-          " #ffff33",
-          " #d2a679",
-          " #4d4dff",
-          " #ffa64d",
-          " #a3a3c2 ",
-          " #ff99ff ",
-        ],
-        borderColor: " #29293d",
-        borderWidth: 1,
-      },
-    ],
-  };
+  function showByPercentage() {
+    let dataArr = [];
+
+    labels.forEach((label) => {
+      dataArr.push(percentageByLabel(label));
+    });
+
+    return {
+      labels: labels,
+      datasets: [
+        {
+          label: "Popularity",
+          data: dataArr,
+          backgroundColor: [
+            " #ff1a1a",
+            " #ff4dff",
+            " #4dd2ff",
+            " #33ff77",
+            " #ffff33",
+            " #d2a679",
+            " #4d4dff",
+            " #ffa64d",
+            " #a3a3c2 ",
+            " #ff99ff ",
+          ],
+          borderColor: " #29293d",
+          borderWidth: 1,
+        },
+      ],
+    };
+  }
+
+  function percentageByLabel(label) {
+    var totalCounter = 0;
+    var inStockCounter = 0;
+
+    toys.map((toy) => {
+      if (toy.labels.includes(label)) {
+        totalCounter++;
+        if (toy.inStock) {
+          inStockCounter++;
+        }
+      }
+    });
+
+    return 100 * (inStockCounter / totalCounter);
+  }
+
+  if (!toys || toys.length === 0) {
+    return <h3>loading...</h3>;
+  }
+
   return (
     <div>
       <button onClick={() => navigate(`/`)}>Back</button>
-      <Pie data={data} />
+      <button onClick={() => setCurrChar(showByPercentage())}>
+        Show by percentage
+      </button>
+      <button onClick={() => setCurrChar(showByPrice())}>Show by price</button>
+      <Pie data={currChar} />
+      <Bar data={currChar} />
     </div>
   );
 }
