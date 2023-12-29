b371ab399c468e48508f333eb384e9a4175f70d8
Iris Yatskaer <iriska1160@gmail.com>
2023-12-29 14:31:51 +0200

    Add async funcs()

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,82 +1,101 @@
-import { storageService } from './async-storage.service.js'
-import { httpService } from './http.service.js'
+import { storageService } from "./async-storage.service.js";
+import { httpService } from "./http.service.js";
 
-const BASE_URL = 'auth/'
-const STORAGE_KEY_LOGGEDIN = 'loggedinUser'
+const BASE_URL = "auth/";
+const STORAGE_KEY_LOGGEDIN = "loggedinUser";
 
 export const userService = {
-    login,
-    logout,
-    signup,
-    getById,
-    getLoggedinUser,
-    updateScore,
-    getEmptyCredentials
-}
+  login,
+  logout,
+  signup,
+  getById,
+  getLoggedinUser,
+  updateScore,
+  getEmptyCredentials,
+};
 
 function getById(userId) {
-    return httpService.get(BASE_URL + userId)
+  return httpService.get(BASE_URL + userId);
 }
 
-function login({ username, password }) {
+async function login({ username, password }) {
+  try {
+    const user = await httpService.post(BASE_URL + "login", {
+      username,
+      password,
+    });
+    if (user) return _setLoggedinUser(user);
+    return Promise.reject("Invalid login");
+  } catch (err) {
+    return Promise.reject("Invalid login");
+  }
 
-    return httpService.post(BASE_URL + 'login', { username, password })
-        .then(user => {
-            if (user) return _setLoggedinUser(user)
-            else return Promise.reject('Invalid login')
-        })
+  // return httpService.post(BASE_URL + 'login', { username, password })
+  //     .then(user => {
+  //         if (user) return _setLoggedinUser(user)
+  //         else return Promise.reject('Invalid login')
+  //     })
 }
 
-function signup({ username, password, fullname }) {
-    const user = { username, password, fullname, score: 10000 }
-    return httpService.post(BASE_URL + 'signup', user)
-        .then(user => {
-            if (user) return _setLoggedinUser(user)
-            else return Promise.reject('Invalid signup')
-        })
+async function signup({ username, password, fullname }) {
+  const user = { username, password, fullname, score: 10000 };
+  try {
+    const signedUser = await httpService.post(BASE_URL + "signup", user);
+    if (signedUser) return _setLoggedinUser(signedUser);
+    return Promise.reject("Invalid signup");
+  } catch {
+    return Promise.reject("Invalid signup");
+  }
+
+  //   return httpService.post(BASE_URL + "signup", user).then((user) => {
+  //     if (user) return _setLoggedinUser(user);
+  //     else return Promise.reject("Invalid signup");
+  //   });
 }
 
+// function updateScore(diff) {
 
+//   if (getLoggedinUser().score + diff < 0) return Promise.reject("No credit");
+//   return httpService.put("user/", { diff }).then((user) => {
+//     _setLoggedinUser(user);
+//     return user.score;
+//   });
+// }
 
-function updateScore(diff) {
-    if (getLoggedinUser().score + diff < 0) return Promise.reject('No credit')
-    return httpService.put('user/', { diff })
-        .then(user => {
-            _setLoggedinUser(user)
-            return user.score
-        })
-}
-
-function logout() {
-    return httpService.post(BASE_URL + 'logout')
-        .then(() => {
-            sessionStorage.removeItem(STORAGE_KEY_LOGGEDIN)
-        })
+async function logout() {
+  try {
+    await httpService.post(BASE_URL + "logout");
+    sessionStorage.removeItem(STORAGE_KEY_LOGGEDIN);
+  } catch (err) {
+    //   return httpService.post(BASE_URL + "logout").then(() => {
+    //     sessionStorage.removeItem(STORAGE_KEY_LOGGEDIN);
+    //   });
+    console.log("err:" + err);
+  }
 }
 
 function getLoggedinUser() {
-    return JSON.parse(sessionStorage.getItem(STORAGE_KEY_LOGGEDIN))
+  return JSON.parse(sessionStorage.getItem(STORAGE_KEY_LOGGEDIN));
 }
 
 function _setLoggedinUser(user) {
-    const userToSave = { _id: user._id, fullname: user.fullname, score: user.score }
-    sessionStorage.setItem(STORAGE_KEY_LOGGEDIN, JSON.stringify(userToSave))
-    return userToSave
+  const userToSave = {
+    _id: user._id,
+    fullname: user.fullname,
+    score: user.score,
+  };
+  sessionStorage.setItem(STORAGE_KEY_LOGGEDIN, JSON.stringify(userToSave));
+  return userToSave;
 }
-
 
 function getEmptyCredentials() {
-    return {
-        username: '',
-        password: '',
-        fullname: ''
-    }
+  return {
+    username: "",
+    password: "",
+    fullname: "",
+  };
 }
-
 
 // Test Data
 // userService.signup({username: 'muki', password: 'muki1', fullname: 'Muki Ja'})
 // userService.login({username: 'muki', password: 'muki1'})
-
-
-
--- src/store/actions/toyActions.js
+++ src/store/actions/toyActions.js
@@ -10,67 +10,108 @@ import {
 } from "../reducers/toyReducer.js";
 import { store } from "../store.js";
 
-export function loadToys() {
+export async function loadToys() {
   store.dispatch({ type: SET_IS_LOADING, isLoading: true });
   const filterBy = store.getState().toyModule.filterBy;
-  return toyService
-    .query(filterBy)
-    .then((toys) => {
-      store.dispatch({ type: SET_TOYS, toys });
-    })
-    .catch((err) => {
-      console.log("toy action -> Cannot load toys", err);
-      throw err;
-    })
-    .finally(() => {
-      store.dispatch({ type: SET_IS_LOADING, isLoading: false });
-    });
+
+  try {
+    const toys = await toyService.query(filterBy);
+    store.dispatch({ type: SET_TOYS, toys });
+  } catch (err) {
+    console.log("toy action -> Cannot load toys", err);
+    throw err;
+  } finally {
+    store.dispatch({ type: SET_IS_LOADING, isLoading: false });
+  }
+
+  // toyService
+  //   .query(filterBy)
+  //   .then((toys) => {
+  //     store.dispatch({ type: SET_TOYS, toys });
+  //   })
+  // .catch((err) => {
+  //   console.log("toy action -> Cannot load toys", err);
+  //   throw err;
+  // })
+  // .finally(() => {
+  //   store.dispatch({ type: SET_IS_LOADING, isLoading: false });
+  // });
 }
 
-export function removeToyOptimistic(toyId) {
+export async function removeToyOptimistic(toyId) {
   store.dispatch({ type: REMOVE_TOY, toyId });
   store.dispatch({ type: SET_IS_LOADING, isLoading: true });
 
-  return toyService
-    .remove(toyId)
-    .catch((err) => {
-      store.dispatch({ type: TOY_UNDO });
-      console.log("toy action -> Cannot remove toy", err);
-      throw err;
-    })
-    .finally(() => {
-      store.dispatch({ type: SET_IS_LOADING, isLoading: false });
-    });
+  try {
+    return await toyService.remove(toyId);
+  } catch (err) {
+    store.dispatch({ type: TOY_UNDO });
+    console.log("toy action -> Cannot remove toy", err);
+    throw err;
+  } finally {
+    store.dispatch({ type: SET_IS_LOADING, isLoading: false });
+  }
+
+  // return toyService
+  //   .remove(toyId)
+  //   .catch((err) => {
+  //     store.dispatch({ type: TOY_UNDO });
+  //     console.log("toy action -> Cannot remove toy", err);
+  //     throw err;
+  //   })
+  //   .finally(() => {
+  //     store.dispatch({ type: SET_IS_LOADING, isLoading: false });
+  //   });
 }
 
-export function removeToy(toyId) {
+export async function removeToy(toyId) {
   store.dispatch({ type: SET_IS_LOADING, isLoading: true });
-  return toyService
-    .remove(toyId)
-    .then(() => {
-      store.dispatch({ type: REMOVE_TOY, toyId });
-    })
-    .catch((err) => {
-      console.log("toy action -> Cannot remove toy", err);
-      throw err;
-    })
-    .finally(() => {
-      store.dispatch({ type: SET_IS_LOADING, isLoading: false });
-    });
+
+  try {
+    await toyService.remove(toyId);
+    store.dispatch({ type: REMOVE_TOY, toyId });
+  } catch (err) {
+    console.log("toy action -> Cannot remove toy", err);
+    throw err;
+  } finally {
+    store.dispatch({ type: SET_IS_LOADING, isLoading: false });
+  }
+
+  // return toyService
+  //   .remove(toyId)
+  //   .then(() => {
+  //     store.dispatch({ type: REMOVE_TOY, toyId });
+  //   })
+  //   .catch((err) => {
+  //     console.log("toy action -> Cannot remove toy", err);
+  //     throw err;
+  //   })
+  //   .finally(() => {
+  //     store.dispatch({ type: SET_IS_LOADING, isLoading: false });
+  //   });
 }
 
-export function saveToy(toy) {
+export async function saveToy(toy) {
   const type = toy._id ? UPDATE_TOY : ADD_TOY;
-  return toyService
-    .save(toy)
-    .then((toyToSave) => {
-      store.dispatch({ type, toy: toyToSave });
-      return toyToSave;
-    })
-    .catch((err) => {
-      console.log("toy action -> Cannot save toy", err);
-      throw err;
-    });
+  try {
+    const toyToSave = await toyService.save(toy);
+    store.dispatch({ type, toy: toyToSave });
+    return toyToSave;
+  } catch (err) {
+    console.log("toy action -> Cannot save toy", err);
+    throw err;
+  }
+
+  // return toyService
+  //   .save(toy)
+  //   .then((toyToSave) => {
+  //     store.dispatch({ type, toy: toyToSave });
+  //     return toyToSave;
+  //   })
+  //   .catch((err) => {
+  //     console.log("toy action -> Cannot save toy", err);
+  //     throw err;
+  //   });
 }
 
 export function setFilterBy(filterBy) {
